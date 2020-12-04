fd49d4cf97c92e240b9b8f2225ee6a36032aa696
bguilas <bryanjguilas@gmail.com>
2020-12-03 21:38:21 -0800

    project finish

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const {hash} = window.location;
+
+//replacing the # of the hash element with empty string
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
